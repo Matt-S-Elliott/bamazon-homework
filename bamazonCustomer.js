1b0dccbca08750c9c97474979b3fa12c65c32f3e
Matthew Elliott <mse951993@gmail.com>
2019-03-25 19:18:29 -0700

    Finished customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,35 +9,84 @@ const connection = mysql.createConnection({
     password: "password",
     database: "bamazon"
 });
+
 connection.connect(err => {
     if (err) throw err;
 });
+buySomething(true);
 
-connection.query("SELECT * FROM products", (err, results) => {
+function printStuff(results) {
     console.table(results);
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "What is the item_id of the item you would like to buy?",
-            name: "choice",
-            validate: choice => {
-                if (parseInt(choice) <= results.length + 1) {
-                    return true;
-                }
-                else return false;
-            }
-        },
-        {
-            type: "input",
-            message: "How many would you like?",
-            name: "quantity",
-            validate: quantity => {
-                if (isNaN(quantity)) {return false;}
-                else return true;
-            }
+    console.log("At anytime, type 'table' to see the items available again.");
+    console.log("Type 'exit' to quit.");
+}
+
+
+function buySomething(firstTime) {
+    connection.query("SELECT * FROM products", (err, results) => {
+        if (firstTime) {
+            printStuff(results);
         }
-    ]).then(response => {
-        let item_chosen = results.find(obj => {return obj.item_id === parseInt(response.choice);});
-        console.log(item_chosen);
-    })
-});
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the item_id of the item you would like to buy?",
+                name: "choice",
+                validate: choice => {
+                    if (choice === "exit") {
+                        connection.end();
+                        process.exit();
+                    }
+                    if (choice === "table") {
+                        printStuff(results);
+                        return false;
+                    }
+                    if (parseInt(choice) <= results.length + 1) {
+                        return true;
+                    }
+                    else return false;
+                }
+            },
+            {
+                type: "input",
+                message: "How many would you like?",
+                name: "quantity",
+                validate: quantity => {
+                    if (quantity === "exit") {
+                        connection.end();
+                        process.exit();
+                    }
+                    if (quantity === "table") {
+                        printStuff(results);
+                        return false;
+                    }
+                    if (isNaN(quantity)) { return false; }
+                    else return true;
+                }
+            }
+        ]).then(response => {
+            let item_chosen = results.find(obj => { return obj.item_id === parseInt(response.choice); });
+            if (response.quantity > item_chosen.stock_quantity) {
+                console.log("Not enought in stock! Sorry...");
+                buySomething(false);
+            } else {
+                let new_quantity = parseInt(item_chosen.stock_quantity) - parseInt(response.quantity);
+                var query = connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: new_quantity
+                        },
+                        {
+                            item_id: response.choice
+                        }
+                    ],
+                    (err, res) => {
+                        console.log(`Sure! That'll be $${(parseInt(response.quantity) * item_chosen.price).toFixed(2)}, please!`);
+                        buySomething(false);
+                    }
+                );
+            }
+        })
+    });
+}
